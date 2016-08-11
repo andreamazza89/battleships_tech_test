e65edc431c58952177c1c03eac284b7075498860
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-08-11 16:29:25 +0100

    Board setup integration test WIP

--- spec/feature/board_setup_spec.rb
+++ spec/feature/board_setup_spec.rb
@@ -1,4 +1,17 @@
-describe 'Feature: Board setup' do
+describe 'Integration: Board setup' do
+
+let(:visualiser) { Visualiser.new }
+let(:test_board) { Board.new }
+
+  it 'Visualiser displays empty board' do 
+    expect { visualiser.show_board(test_board) }.to output(EMPTY_BOARD).to_stdout_from_any_process
+  end
+
+  it 'User can place a ship in a board location' do 
+    coordinates = { x: 3, y: 2 }
+    test_board.add_ship(coordinates, :horizontal)
+    expect { visualiser.show_board(test_board) }.to output(ONE_SHIP).to_stdout_from_any_process
+  end
 
   EMPTY_BOARD = 
 "   A B C D E F G H I J
@@ -14,11 +27,17 @@ describe 'Feature: Board setup' do
 10|-------------------
 "
 
-  it 'Visualiser displays empty board as required' do
-    test_board  = Board.new
-    visualiser = Visualiser.new
-
-    expect { visualiser.show_board(test_board) }.to output(EMPTY_BOARD).to_stdout_from_any_process
-  end
-
+  ONE_SHIP = 
+"   A B C D E F G H I J
+ 1|-------------------
+ 2|-------------------
+ 3|------S------------
+ 4|-------------------
+ 5|-------------------
+ 6|-------------------
+ 7|-------------------
+ 8|-------------------
+ 9|-------------------
+10|-------------------
+"
 end
